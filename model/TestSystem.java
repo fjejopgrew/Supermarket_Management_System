0434386b253f14ba8612076b0634e6d7a43e2892
Hang Gao <hgao19@hawk.iit.edu>
2016-02-24 12:39:02 +0800

    Create TestSystem.java

---
+++ model/TestSystem.java
@@ -0,0 +1,13 @@
+package shop.management.system.model;
+
+import shop.management.system.view.MainMenu;
+
+public class TestSystem
+{
+
+	public static void main(String[] args)
+	{
+		MainMenu m = new MainMenu();
+		m.mainMenu();
+	}
+}
